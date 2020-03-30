30ee122e785199aabb78293f1b493d16d49bb733
gabrielacorona <gabriela.corona03@gmail.com>
2020-03-30 08:06:11 -0600

    final version

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
+function checkElement() {
+    let ul = document.querySelector('ul');
+    ul.addEventListener('click', (event) => {
+        //console.log(event.target.parentElement.parentElement);
+       
+        if (event.target.matches('.checkList')) {
+            //console.log(event.target.parentElement.parentElement);
+            let li = event.target.parentElement.parentElement;
+            if(li.className = "added"){
+                li.classList.add('checkElement');
+                li.classList.remove('added')
+            }
+        }
+        
+        if(event.target.matches('.deleteList')){
+            //console.log(event.target.parentElement);
+            event.target.parentElement.parentElement.remove();
+        }
 
+       
+    });
 
+}
 
 function addElement() {
+    let form = document.getElementById("formComment")
     let submit = document.getElementById("addElement");
     let element = document.getElementById("inputBox");
-    let listSect = document.getElementById("unorderedList");
+    let ul = document.querySelector('ul');
 
-    submit.addEventListener('click', (event) => {
-
-        let ul = document.querySelector('ul');
-
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
         if (element.value != "") {
             ul.innerHTML +=
                 `
-            <li>
-                ${element.value}
+            <li class = "added">
                 <div>
-                <button> Check </button>
-                <button> Delete </button>
+                    <p>${element.value}</p>
+                    <button class = "checkList"> Check </button>
+                    <button class = "deleteList"> Delete </button>
                 </div>
             </li>
             `;
@@ -28,6 +48,7 @@ function addElement() {
 
 function init() {
     addElement();
+    checkElement()
 
 }
 
